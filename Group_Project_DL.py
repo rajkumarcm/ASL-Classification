383ead72ab4296771cafba4f0bcf1b01025070d4
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2023-04-02 00:26:15 -0400

    Debug dataset and data loaderr iterator

--- Group_Project_DL.py
+++ Group_Project_DL.py
@@ -1,7 +1,16 @@
+#%%
 import numpy as np
 import torch
+from torch.utils.data import Dataset
+import torchvision
+from torchvision import transforms
+from torchmetrics import Accuracy
+import re
 import pandas as pd
 import os
+from os import chdir, listdir
+from os.path import abspath, join
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
@@ -9,42 +18,121 @@ import random
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset
-from torchvision import transforms
+
 from torch.utils.data import DataLoader
+INTERPOLATION_MODE = transforms.InterpolationMode.NEAREST
 
+#%%
+# dataset_dir = os.path.abspath('/Users/daqian.dang/Desktop/DATS 6303/Project/dataset5/')
+DATA_DIR = r"C:\Users\Rajkumar\Downloads\ASL\dataset5\collated"
 
+#%%
 # Define function to store labels and images into dictionary
+# def get_labels_images(path):
+#     directories = []
+#     labels = []
+#     images = []
+#
+#     for directory in os.listdir(dataset_dir):
+#         dir_path = os.path.join(dataset_dir, directory)
+#
+#         for label in os.listdir(dir_path):
+#             label_path = os.path.join(dir_path, label)
+#
+#             for image in os.listdir(label_path):
+#                 directories.append(directory)
+#                 labels.append(label)
+#                 images.append(os.path.join(directory, label, image))
+#
+#     print(len(images), len(labels), len(directories))
+#     return pd.DataFrame({'directories': directories, 'labels': labels, 'images': images})
 
-def get_labels_images(path):
-    directories = []
-    labels = []
-    images = []
+#%%
+# Data augmentation
+#Define the data augmentation transforms for the training set
+def normalize(X):
+    return X * 1./255
 
-    for directory in os.listdir(dataset_dir):
-        dir_path = os.path.join(dataset_dir, directory)
-        if not os.path.isdir(dir_path):
-            continue
+train_transforms = transforms.Compose([
+    transforms.Resize((224, 224), interpolation=INTERPOLATION_MODE), #resize 224*224
+    transforms.RandomHorizontalFlip(), #random horizontal flip
+    # Vertical flip not required
+    # transforms.RandomVerticalFlip(), #random vertical flip
+    transforms.RandomRotation(15, interpolation=INTERPOLATION_MODE), #random rotation by 15 degrees
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2), # Color jitter
+    transforms.ToTensor(), # transform to corvent the PIL image to a PyTorch tensor
+    transforms.Lambda(normalize) # scale image values by dividing them by 255
+])
 
-        for label in os.listdir(dir_path):
-            label_path = os.path.join(dir_path, label)
-            if not os.path.isdir(label_path):
-                continue
-
-            for image in os.listdir(label_path):
-                directories.append(directory)
-                labels.append(label)
-                images.append(os.path.join(directory, label, image))
-
-    print(len(images), len(labels), len(directories))
-    return pd.DataFrame({'directories': directories, 'labels': labels, 'images': images})
-
-dataset_dir = os.path.abspath('/Users/daqian.dang/Desktop/DATS 6303/Project/dataset5/')
-data = get_labels_images(dataset_dir)
+#Define the data augmentation transforms for the validation and test sets
+val_test_transforms = transforms.Compose([
+    transforms.Resize((224, 224), interpolation=INTERPOLATION_MODE),
+    transforms.ToTensor(),
+    transforms.Lambda(normalize)
+])
 
 
-# Show random image samples (mixed color images and non-color images)
+#%%
+class ImageDataset(Dataset):
+    def __init__(self, data_dir, transforms, training, split_indices=None):
+        self.data_dir = data_dir
+        self.transforms = transforms
+        # self.is_color = lambda x: len(re.findall("color", x)) > 0
+        self.le = LabelEncoder()
+        self.training = training
 
+        n_imgs = 0
+        # join_path = lambda x: join(self.data_dir, join(x[0], x[1]))
+
+        imgs = []
+        labels = []
+        for char_ in listdir(self.data_dir):
+            fnames = listdir(join(self.data_dir, char_))
+            # fnames = list(filter(self.is_color, fnames))
+            color_filter_mask = [len(re.findall("color", fname)) > 0 for fname in fnames]
+            fnames = np.array(fnames)[color_filter_mask]
+            fnames = [join(self.data_dir, join(char_, fname)) for fname in fnames]
+            # fnames = list(map(join_path, zip([char_]*len(fnames), fnames)))
+            imgs = np.r_[imgs, fnames]
+            labels = np.r_[labels, [char_]*len(fnames)]
+            n_imgs += len(fnames)
+        self.n_imgs = n_imgs
+        labels = self.le.fit_transform(labels)
+        indices = list(range(n_imgs))
+        labels = labels[indices]
+        imgs = imgs[indices]
+        self.df = pd.DataFrame({"img": imgs, "label": labels})
+        if not split_indices:
+            split_indices = list(range(self.df.shape[0]))
+            tr_indices, vl_indices, tr_labels, vl_labels = train_test_split(split_indices, labels,
+                                                                            test_size=0.3, stratify=labels)
+            self.tr_indices = tr_indices
+            self.vl_indices = vl_indices
+        else:
+            tr_indices, vl_indices = split_indices
+
+        self.tr_data, self.vl_data = self.df.iloc[tr_indices].reset_index(), \
+                                     self.df.iloc[vl_indices].reset_index()
+
+    def __len__(self):
+        if self.training:
+            return self.tr_data.shape[0]
+        else:
+            return self.vl_data.shape[0]
+
+    def __getitem__(self, idx):
+        if self.training:
+            img_fname, label = self.tr_data.loc[idx, "img"], self.df.loc[idx, "label"]
+        else:
+            img_fname, label = self.vl_data.loc[idx, "img"], self.df.loc[idx, "label"]
+        image = Image.open(img_fname)
+        image = torchvision.transforms.PILToTensor()(image)
+        image = torchvision.transforms.ConvertImageDtype(dtype=torch.float32)(image)
+        image = torchvision.transforms.ToPILImage()(image)
+        image = self.transforms(image)
+        return image, label
+
+#%%
 def random_image_samples(data: pd.DataFrame, num_rows: int=4, num_columns: int=4):
     num_images = num_rows * num_columns
     random_indices = random.sample(range(len(data)), num_images)
@@ -53,49 +141,32 @@ def random_image_samples(data: pd.DataFrame, num_rows: int=4, num_columns: int=4
     plt.figure(figsize=(15, 15))
 
     for i, (index, row) in enumerate(random_images.iterrows()):
-        image_path = os.path.join(dataset_dir, row['images'])
+        image_path = os.path.join(DATA_DIR, row['img'])
         image = imread(image_path)
-        label = row['labels']
+        label = row['label']
 
         plt.subplot(num_rows, num_columns, i + 1)
         plt.imshow(image)
-        plt.title(f"Label: {label}")
+        plt.title(f"Label: {tr_data.le.inverse_transform([label])[0]}")
         plt.axis('off')
 
     plt.tight_layout()
     plt.show()
 
-random_image_samples(data, num_rows=4, num_columns=4)
+#%%
+# random_image_samples(data, num_rows=4, num_columns=4)
 
+tr_data = ImageDataset(DATA_DIR, train_transforms, training=True)
+split_indices = (tr_data.tr_indices, tr_data.vl_indices)
+vl_data = ImageDataset(DATA_DIR, val_test_transforms, training=False,
+                       split_indices=split_indices)
 
-# Show random image samples
-
-color_data = data[data['images'].map(
-    lambda x: True if x.find('color')!=-1 else False)].reset_index(drop=True)
-deep_data = data[data['images'].map(
-    lambda x: True if x.find('color')==-1 else False)].reset_index(drop=True)
-data.shape[0]==(color_data.shape[0] + deep_data.shape[0])
-
-random_image_samples(color_data, num_rows=4, num_columns=4) # color images only
-random_image_samples(deep_data, num_rows=4, num_columns=4) # non-color images only
-
-
-# Image size
-def select_random_image(data: pd.DataFrame, dataset_dir: str):
-    random_row = data.sample()
-    image_path = os.path.join(dataset_dir, random_row['images'].values[0])
-    image = Image.open(image_path)
-
-    return image
-
-random_image = select_random_image(data, dataset_dir)
-shape = random_image.size + (len(random_image.getbands()),)
-print(shape)
-
+#%%
+random_image_samples(tr_data.tr_data, num_rows=4, num_columns=4) # color images only
 
 # Check the image label values if they are balanced or imbalanced
-label_counts = data['labels'].value_counts(normalize=True)
-print(label_counts)
+label_counts = tr_data.tr_data.label.value_counts(normalize=False)
+# print(label_counts)
 
 plt.figure(figsize=(10, 5))
 plt.bar(label_counts.index, label_counts.values)
@@ -105,73 +176,78 @@ plt.ylabel('Frequency')
 plt.title('Label Balance Check')
 plt.show()
 
-# After checking and plotting the balance of the image label values, the dataset is fairly balanced.
-
-
-# Rename data labels to target
-target = data['labels']
-
-# Encode labels
-label_encoder = LabelEncoder()
-label_encoder.fit(target)
-data['encoded_labels'] = label_encoder.transform(target)
-
-
-# Split dataset into train, validation, and test sets
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
-train_data, val_data = train_test_split(train_data, test_size=0.2, random_state=42)
-
-
-# Data augmentation
-#Define the data augmentation transforms for the training set
-train_transforms = transforms.Compose([
-    transforms.Resize((224, 224)), #resize 224*224
-    transforms.RandomHorizontalFlip(), #random horizontal flip
-    transforms.RandomVerticalFlip(), #random vertical flip
-    transforms.RandomRotation(15), #random rotation by 15 degrees
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2), # Color jitter
-    transforms.ToTensor(), # transform to corvent the PIL image to a PyTorch tensor
-    transforms.Lambda(lambda x: x / 255.0) # scale image values by dividing them by 255
-])
-
-#Define the data augmentation transforms for the validation and test sets
-val_test_transforms = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Lambda(lambda x: x / 255.0)
-])
-
-class CustomDataset(Dataset):
-    def __init__(self, data: pd.DataFrame, dataset_dir: str, transform=None):
-        self.data = data
-        self.dataset_dir = dataset_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, i):
-        image_name = os.path.join(self.dataset_dir, self.data.iloc[i]['images'])
-        image = Image.open(image_name).convert('RGB')
-        label = self.data.iloc[i]['encoded_labels']
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
-
-# Create CustomDataset objects for train, validation, and test sets
-train_dataset = CustomDataset(train_data, dataset_dir, transform=train_transforms)
-val_dataset = CustomDataset(val_data, dataset_dir, transform=val_test_transforms)
-test_dataset = CustomDataset(test_data, dataset_dir, transform=val_test_transforms)
-
 
 # Employ DataLoader as prefetch for training
 BATCH_SIZE = 32
 NUM_WORKERS = 4  # this is parameter that determines the number of worker processes to use for loading the data
 PREFETCH_FACTOR = 2  # this determines how many samples will be loaded per worker process during the prefetching
 
-new_train = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
-new_val = DataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
-new_test = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
+tr_data.__getitem__(0)
+tr_loader = DataLoader(tr_data, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
+vl_loader = DataLoader(vl_data, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
+# new_test = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
+N_CLASSES = len(tr_data.tr_data.label.unique())
+
+model = torch.nn.Sequential(
+    torch.nn.Conv2d(3, 16, 3, 1, 1, 'replicate'), # 222x222x16
+    torch.nn.ReLU(),
+    torch.nn.MaxPool2d(2, 1), # 111x111x16
+    torch.nn.Conv2d(16, 32, 3, 1, 1, 'replicate'), # 109x109x32
+    torch.nn.ReLU(),
+    torch.nn.MaxPool2d(2, 1), # 54x54x32
+    torch.nn.Conv2d(32, 64, 3, 1, 1, 'replicate'), # 52x52x64
+    torch.nn.ReLU(),
+    torch.nn.MaxPool2d(2, 1), # 26x26x64
+    torch.nn.Flatten(),
+    torch.nn.Linear(26*26*64, N_CLASSES)
+    # Ignored Softmax since the loss has this integrated.
+)
+
+LR = 1e-3
+N_EPOCHS = 10
+
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+criterion = torch.nn.CrossEntropyLoss()
+acc = Accuracy(task='multiclass', num_classes=N_CLASSES)
+print("Starting training loop...")
+for epoch in range(N_EPOCHS):
+    loss_train = 0
+    model.train()
+    for i, (X_train, y_train) in enumerate(tr_loader):
+        optimizer.zero_grad()
+        logits = model(X_train)
+        loss = criterion(logits, y_train)
+        loss.backward()
+        optimizer.step()
+        loss_train += loss.item()
+
+    model.eval()
+    with torch.no_grad():
+        loss_val = 0
+        count = 0
+        for i, (X_val, y_val) in enumerate(vl_loader):
+            y_val_pred = model(X_val)
+            loss = criterion(y_val_pred, y_val)
+            loss_val += loss.item()
+
+    print("Epoch {} | Train Loss {:.5f}, Train Acc {:.2f} - Test Loss {:.5f}, Test Acc {:.2f}".format(
+        epoch, loss_train/BATCH_SIZE, acc(torch.nn.Softmax(logits), y_train),
+        loss_val, acc(X_val, y_val)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
