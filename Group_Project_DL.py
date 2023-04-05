b97faf79e4442736a7b5ce5fe7b7deade3370072
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2023-04-05 19:49:26 -0400

    Fixed dataset and accuracy problems

--- Group_Project_DL.py
+++ Group_Project_DL.py
@@ -25,61 +25,16 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 INTERPOLATION_MODE = transforms.InterpolationMode.NEAREST
 
-
 class ImageDataset(Dataset):
-    def __init__(self, data_dir, transforms, training, split_indices=None):
-        self.data_dir = data_dir
+    def __init__(self, data, transforms):
+        self.data = data.loc[:, ["img", "label"]].reset_index()
         self.transforms = transforms
-        # self.is_color = lambda x: len(re.findall("color", x)) > 0
-        self.le = LabelEncoder()
-        self.training = training
-
-        n_imgs = 0
-        # join_path = lambda x: join(self.data_dir, join(x[0], x[1]))
-
-        imgs = []
-        labels = []
-        for char_ in listdir(self.data_dir):
-            fnames = listdir(join(self.data_dir, char_))
-            # fnames = list(filter(self.is_color, fnames))
-            color_filter_mask = [len(re.findall("color", fname)) > 0 for fname in fnames]
-            fnames = np.array(fnames)[color_filter_mask]
-            fnames = [join(self.data_dir, join(char_, fname)) for fname in fnames]
-            # fnames = list(map(join_path, zip([char_]*len(fnames), fnames)))
-            imgs = np.r_[imgs, fnames]
-            labels = np.r_[labels, [char_] * len(fnames)]
-            n_imgs += len(fnames)
-        self.n_imgs = n_imgs
-        labels = self.le.fit_transform(labels)
-        indices = list(range(n_imgs))
-        labels = labels[indices]
-        imgs = imgs[indices]
-        self.df = pd.DataFrame({"img": imgs, "label": labels})
-        if not split_indices:
-            split_indices = list(range(self.df.shape[0]))
-            tr_indices, vl_indices, tr_labels, vl_labels = train_test_split(split_indices,
-                                                                            labels,
-                                                                            test_size=0.3,
-                                                                            stratify=labels)
-            self.tr_indices = tr_indices
-            self.vl_indices = vl_indices
-        else:
-            tr_indices, vl_indices = split_indices
-
-        self.tr_data, self.vl_data = self.df.iloc[tr_indices].reset_index(), \
-            self.df.iloc[vl_indices].reset_index()
 
     def __len__(self):
-        if self.training:
-            return self.tr_data.shape[0]
-        else:
-            return self.vl_data.shape[0]
+        return self.data.shape[0]
 
     def __getitem__(self, idx):
-        if self.training:
-            img_fname, label = self.tr_data.loc[idx, "img"], self.df.loc[idx, "label"]
-        else:
-            img_fname, label = self.vl_data.loc[idx, "img"], self.df.loc[idx, "label"]
+        img_fname, label = self.data.iloc[idx]["img"], self.data.iloc[idx]["label"]
         image = Image.open(img_fname)
         image = torchvision.transforms.PILToTensor()(image)
         image = torchvision.transforms.ConvertImageDtype(dtype=torch.float32)(image)
@@ -91,21 +46,46 @@ class ASLRecognition:
     # Data augmentation
     #Define the data augmentation transforms for the training set
     def normalize(self, X):
-        return X * 1. / 255
+        return X * 1./255
+
+    def load_filenames_df(self, data_dir, le):
+        total_n_imgs = 0
+        # join_path = lambda x: join(self.data_dir, join(x[0], x[1]))
+
+        total_imgs = []
+        total_labels = []
+        for char_ in listdir(data_dir):
+            fnames = listdir(join(data_dir, char_))
+            color_filter_mask = [len(re.findall("color", fname)) > 0 for fname in fnames]
+            fnames = np.array(fnames)[color_filter_mask]
+            fnames = [join(data_dir, join(char_, fname)) for fname in fnames]
+            total_imgs = np.r_[total_imgs, fnames]
+            total_labels = np.r_[total_labels, [char_] * len(fnames)]
+            total_n_imgs += len(fnames)
+        total_n_imgs = total_n_imgs
+        total_labels = le.fit_transform(total_labels)
+        indices = list(range(total_n_imgs))
+        total_labels = total_labels[indices]
+        total_imgs = total_imgs[indices]
+        df = pd.DataFrame({"img": total_imgs, "label": total_labels})
+        tr_data, ts_data, tr_labels, ts_labels = train_test_split(df, df.label, test_size=0.3, stratify=total_labels)
+        tr_data, vl_data, tr_labels, vl_labels = train_test_split(tr_data, tr_data.label, test_size=0.3, stratify=tr_labels)
+        return tr_data, vl_data, ts_data, tr_labels, vl_labels, ts_labels
 
     def __init__(self):
         # dataset_dir = os.path.abspath('/Users/daqian.dang/Desktop/DATS 6303/Project/dataset5/')
-        # DATA_DIR = r"C:\Users\Rajkumar\Downloads\ASL\dataset5\collated"
-        DATA_DIR = r"/home/ubuntu/ASL_Data/dataset5/collated"
+        DATA_DIR = r"C:\Users\Rajkumar\Downloads\ASL\dataset5\collated"
+        # DATA_DIR = r"/home/ubuntu/ASL_Data/dataset5/collated"
         NUM_WORKERS = 8
-        PREFETCH_FACTOR = 3
+        PREFETCH_FACTOR = 10
         self.BATCH_SIZE = 200
         self.LR = 1e-3
         self.N_EPOCHS = 10
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.le = LabelEncoder()
 
         train_transforms = transforms.Compose([
-            transforms.Resize((224, 224), interpolation=INTERPOLATION_MODE), #resize 224*224
+            transforms.Resize((56, 56), interpolation=INTERPOLATION_MODE), #resize 224*224
             transforms.RandomHorizontalFlip(), #random horizontal flip
             # Vertical flip not required
             # transforms.RandomVerticalFlip(), #random vertical flip
@@ -117,21 +97,25 @@ class ASLRecognition:
 
         #Define the data augmentation transforms for the validation and test sets
         val_test_transforms = transforms.Compose([
-            transforms.Resize((224, 224), interpolation=INTERPOLATION_MODE),
+            transforms.Resize((56, 56), interpolation=INTERPOLATION_MODE),
             transforms.ToTensor(),
             transforms.ConvertImageDtype(torch.float32)
         ])
 
-        tr_data = ImageDataset(DATA_DIR, train_transforms, training=True)
-        split_indices = (tr_data.tr_indices, tr_data.vl_indices)
-        vl_data = ImageDataset(DATA_DIR, val_test_transforms, training=False,
-                               split_indices=split_indices)
-        self.le = tr_data.le
-        self.N_CLASSES = len(tr_data.tr_data.label.unique())
-        self.tr_loader = DataLoader(tr_data, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
-                               prefetch_factor=PREFETCH_FACTOR)
-        self.vl_loader = DataLoader(vl_data, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
-                               prefetch_factor=PREFETCH_FACTOR)
+        tr_data, vl_data, ts_data, tr_labels, vl_labels, ts_labels = self.load_filenames_df(DATA_DIR, self.le)
+        tr_dset = ImageDataset(tr_data, train_transforms)
+        vl_dset = ImageDataset(vl_data, val_test_transforms)
+        ts_dset = ImageDataset(ts_data, val_test_transforms)
+
+        self.N_CLASSES = len(tr_data.label.unique())
+        self.tr_loader = DataLoader(tr_dset, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
+                                    prefetch_factor=PREFETCH_FACTOR)
+        self.vl_loader = DataLoader(vl_dset, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
+                                    prefetch_factor=PREFETCH_FACTOR)
+        # for i, (X_tmp, y_tmp) in enumerate(vl_dset):
+        #     print(i)
+        self.ts_loader = DataLoader(ts_dset, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
+                                    prefetch_factor=PREFETCH_FACTOR)
 
     #%%
     def random_image_samples(self, data, num_rows=4, num_columns=4):
@@ -159,17 +143,17 @@ class ASLRecognition:
 
         # new_test = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
         self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 16, (3, 3), 1, 1, padding_mode='replicate'), # 224x224x16
+            torch.nn.Conv2d(3, 16, (3, 3), 1, 1, padding_mode='replicate'), # 56x56x16
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d((2, 2)),  # 112x112x16
-            torch.nn.Conv2d(16, 32, (3, 3), 1, 1, padding_mode='replicate'), # 112x112x32
+            torch.nn.MaxPool2d((2, 2)),  # 28x28x16
+            torch.nn.Conv2d(16, 32, (3, 3), 1, 1, padding_mode='replicate'), # 28x28x32
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d((2, 2)),  # 56x56x32
-            torch.nn.Conv2d(32, 64, (3, 3), 1, 1, padding_mode='replicate'), # 28x28x64
+            torch.nn.MaxPool2d((2, 2)),  # 14x14x32
+            torch.nn.Conv2d(32, 64, (3, 3), 1, 1, padding_mode='replicate'), # 14x14x64
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d((2, 2)),  # 28x28x64
+            torch.nn.MaxPool2d((2, 2)),  # 7x7x64
             torch.nn.Flatten(),
-            torch.nn.Linear(28*28*64, self.N_CLASSES)
+            torch.nn.Linear(7*7*64, self.N_CLASSES)
             # Ignored Softmax since the loss has this integrated.
         ).cuda()
         return self.model
