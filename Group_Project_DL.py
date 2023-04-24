f0f5bcc8ab8826ae7da3bd9081b4ff320ee1cef9
Andi Ren <82448660+AndiRen@users.noreply.github.com>
2023-04-23 20:29:26 -0400

    Andy optimization (#4)
    Improvements
    -Added early stopping
    -Added LR scheduler
    -Added model save point
    -Included seeding for reproducibility
    -Separated test function from training/validation loop
    -Provided explicit dimension to nn.Softmax
    -Other formatting changes
    
    Optimizations
    -Added dropout (0.3)
    -Added L2 regularization (0.001)
    -Changed kernal size (5x5)
    -Changed batch size (64)
    -Final best model with Test macro F1 of 0.9874

--- Group_Project_DL.py
+++ Group_Project_DL.py
@@ -10,6 +10,7 @@ torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 from torch.utils.data import Dataset
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torchvision
 from torchvision import transforms
 from torchsummary import summary
@@ -22,6 +23,9 @@ import random
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from time import time
+import random
 INTERPOLATION_MODE = transforms.InterpolationMode.NEAREST
 
 
@@ -44,6 +48,12 @@ class ImageDataset(Dataset):
         return image, label
 
 #%%
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
 class ASLRecognition:
     # Data augmentation
     #Define the data augmentation transforms for the training set
@@ -70,23 +80,40 @@ class ASLRecognition:
         total_labels = total_labels[indices]
         total_imgs = total_imgs[indices]
         df = pd.DataFrame({"img": total_imgs, "label": total_labels})
-        tr_data, ts_data, tr_labels, ts_labels = train_test_split(df, df.label, test_size=0.3, stratify=total_labels)
-        tr_data, vl_data, tr_labels, vl_labels = train_test_split(tr_data, tr_data.label, test_size=0.3, stratify=tr_labels)
+        tr_data, ts_data, tr_labels, ts_labels = train_test_split(df, df.label, test_size=0.3, stratify=total_labels, random_state=self.random_seed)
+        tr_data, vl_data, tr_labels, vl_labels = train_test_split(tr_data, tr_data.label, test_size=0.3, stratify=tr_labels, random_state=self.random_seed)
         return tr_data, vl_data, ts_data, tr_labels, vl_labels, ts_labels
-
+    
     def __init__(self):
         # dataset_dir = os.path.abspath('/Users/daqian.dang/Desktop/DATS 6303/Project/dataset5/')
-        DATA_DIR = r"C:\Users\Rajkumar\Downloads\ASL\dataset5\collated"
+        
+        DATA_DIR = r"D:/GWU/DATS-6303/project/archive/dataset5/collated"
         # DATA_DIR = r"/home/ubuntu/ASL_Data/dataset5/collated"
         NUM_WORKERS = 8
         PREFETCH_FACTOR = 30
-        self.BATCH_SIZE = 1000
-        self.LR = 1e-3
-        self.N_EPOCHS = 10
+        self.BATCH_SIZE = 64 # *****
+        self.LR = 1e-3 # *****
+        self.N_EPOCHS = 300 # *****
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.le = LabelEncoder()
         self.IMG_SIZE = 100
-
+        self.filename = "model_optimization_5.pt"
+        self.SAVE_DIR = f"D:/GWU/DATS-6303/project/saved_models/{self.filename}"
+        self.random_seed = 3388
+        
+        # Setup for reproducability *****
+        os.environ['PYTHONHASHSEED'] = str(self.random_seed)
+        torch.manual_seed(self.random_seed)
+        torch.cuda.manual_seed(self.random_seed)
+        # torch.use_deterministic_algorithms(True)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        random.seed(self.random_seed)
+        np.random.seed(self.random_seed)
+        g = torch.Generator()
+        g.manual_seed(self.random_seed)
+        # *****
+        
         train_transforms = transforms.Compose([
             transforms.Resize((self.IMG_SIZE, self.IMG_SIZE), interpolation=INTERPOLATION_MODE), #resize 224*224
             transforms.RandomHorizontalFlip(), #random horizontal flip
@@ -112,13 +139,13 @@ class ASLRecognition:
 
         self.N_CLASSES = len(tr_data.label.unique())
         self.tr_loader = DataLoader(tr_dset, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
-                                    prefetch_factor=PREFETCH_FACTOR)
+                                    prefetch_factor=PREFETCH_FACTOR, worker_init_fn=seed_worker, generator=g)
         self.vl_loader = DataLoader(vl_dset, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
-                                    prefetch_factor=PREFETCH_FACTOR)
+                                    prefetch_factor=PREFETCH_FACTOR, worker_init_fn=seed_worker, generator=g)
         # for i, (X_tmp, y_tmp) in enumerate(vl_dset):
         #     print(i)
         self.ts_loader = DataLoader(ts_dset, batch_size=self.BATCH_SIZE, num_workers=NUM_WORKERS,
-                                    prefetch_factor=PREFETCH_FACTOR)
+                                    prefetch_factor=PREFETCH_FACTOR, worker_init_fn=seed_worker, generator=g)
 
 
     def random_image_samples(self, data, num_rows=4, num_columns=4):
@@ -145,27 +172,62 @@ class ASLRecognition:
         last_input_shape = final_layer_img_size * final_layer_img_size * 64
         # new_test = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, prefetch_factor=PREFETCH_FACTOR)
         model = torch.nn.Sequential(
-                torch.nn.Conv2d(3, 16, 3, padding='same', padding_mode='replicate'), # 56x56x16
+                torch.nn.Conv2d(3, 16, 5, padding='same', padding_mode='replicate'), # 56x56x16
                 torch.nn.ReLU(),
                 torch.nn.MaxPool2d((2, 2)),  # 28x28x16
-                torch.nn.Conv2d(16, 32, 3, padding='same', padding_mode='replicate',
+                
+                # *****
+                torch.nn.Dropout(0.3),
+                # *****
+                
+                torch.nn.Conv2d(16, 32, 5, padding='same', padding_mode='replicate',
                                 groups=2), # 28x28x32
                 torch.nn.ReLU(),
                 torch.nn.MaxPool2d((2, 2)),  # 14x14x32
-                torch.nn.Conv2d(32, 64, 3, padding='same', padding_mode='replicate',
+                
+                # *****
+                torch.nn.Dropout(0.3),
+                # *****
+                
+                torch.nn.Conv2d(32, 64, 5, padding='same', padding_mode='replicate',
                                 groups=2), # 14x14x64
                 torch.nn.ReLU(),
                 torch.nn.MaxPool2d((2, 2)),  # 7x7x64
+                
+                # *****
+                torch.nn.Dropout(0.3),
+                # *****
+                
                 torch.nn.Flatten(),
-                torch.nn.Linear(last_input_shape, self.N_CLASSES)
+                
+                # *****
+                torch.nn.Linear(last_input_shape, 128),
+                torch.nn.ReLU(),
+                torch.nn.Dropout(0.3),
+                # *****
+                
+                torch.nn.Linear(128, self.N_CLASSES)
                 # Ignored Softmax since the loss has this integrated.
         )
         return model
 
     def fit(self, model):
-        optimizer = torch.optim.Adam(model.parameters(), lr=self.LR)
+        start_time = time()
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.LR, weight_decay=0.001) # *****
         criterion = torch.nn.CrossEntropyLoss().cuda()
         acc = Accuracy(task='multiclass', num_classes=self.N_CLASSES).to(self.device)
+        # f1_macro = F1Score(task='multiclass', num_classes=self.N_CLASSES, average='macro').to(self.device)
+        
+        # Initialize test loss threshold for early stopping and parameters to track *****
+        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True) # I think mode should be min for loss?
+        early_stopping_patience = 30          
+        current_patience = early_stopping_patience
+        early_stoping_sensitivity = 4
+        best_validation_loss = round(1000000., early_stoping_sensitivity) # made this arbitrarily large so that the first round of training should always succeed
+        # current_learning_rate = self.LR
+        best_validation_score = 0.0000000001
+        #***** 
+        
         print("Starting training loop...")
         model.cuda()
         model.train()
@@ -191,23 +253,67 @@ class ASLRecognition:
                     vl_logits = model(imgs)
                     loss = criterion(vl_logits, vl_labels)
                     loss_val += loss.item()
+            
+            # Get evaluation metrics for printing
+            tr_preds = torch.argmax(torch.nn.Softmax(dim=1)(tr_logits), axis=1)
+            val_preds = torch.argmax(torch.nn.Softmax(dim=1)(vl_logits), axis=1)
+            acc_tr = acc(tr_preds, tr_labels)
+            acc_val = acc(val_preds, vl_labels)
+            train_loss_batch = round(loss_train / self.BATCH_SIZE, 5)
+            val_loss_batch = round(loss_val / self.BATCH_SIZE, 5)
+            
+            print(f"Epoch {epoch+1} | Train Loss {train_loss_batch}, Train Acc {acc_tr} - Validation Loss {val_loss_batch}, Validation Acc {acc_val}")
+        
+            # EARLY STOPPING and LEARNING RATE SCHEDULING *****
+            # Check if the loss is going down
+            if round(loss_val, early_stoping_sensitivity) >= best_validation_loss:
+                # If loss not decerasing, remove one level of patience and drop learning rate
+                current_patience -=1
+                print(f"early stopping validation loss check activated, early stopping patience remaing: {current_patience}")
+            else:
+                # If loss is decreasing, update lowest loss and reset ES patience
+                best_validation_loss = loss_val
+                current_patience = early_stopping_patience
+            
+            # If ES patience exhausted, stop training and print ES message
+            if current_patience == 0:
+                print(f"early stopping implemented at Epoch {epoch}")
+                break
+            #*****
+            
+            # Include LR scheduler *****
+            scheduler.step(loss_val)
+            #*****
 
-            print("Epoch {} | Train Loss {:.5f}, Train Acc {:.2f} - Test Loss {:.5f}, Test Acc {:.2f}".format(
-                epoch, loss_train / self.BATCH_SIZE, acc(torch.argmax(torch.nn.Softmax()(tr_logits), axis=1), tr_labels),
-                loss_val, acc(torch.argmax(torch.nn.Softmax()(vl_logits), axis=1), vl_labels)))
-
-            with torch.no_grad():
-                ts_pred = []
-                ts_labels = []
-                model.eval()
-                for i, (X_test, y_test) in enumerate(self.ts_loader):
-                    pred = torch.nn.Softmax()(model(X_test.to("cuda")))
-                    pred = np.argmax(pred.cpu().detach().numpy(), axis=1)
-                    ts_pred = np.r_[ts_pred, np.ravel(pred)]
-                    ts_labels = np.r_[ts_labels, np.ravel(y_test.cpu())]
-                f1_score(y_true=ts_labels, y_pred=ts_pred, labels=np.unique(ts_labels), average='macro')
-
-
+            # Saving best performing model *****
+            if acc_val > best_validation_score or (acc_val == 1.0000 and acc_tr == 1.0000):
+                best_validation_score = acc_val
+                torch.save(model.state_dict(), self.SAVE_DIR)
+                print("**Model Saved**")
+            # *****
+                
+        print(f"Total time taken: {time()-start_time}")
+        
+    def test(self, model, used_saved=False):
+        print("Starting testing...")
+        if used_saved == True:
+            model.load_state_dict(torch.load(self.SAVE_DIR))
+        model.cuda()
+        model.eval()
+        with torch.no_grad():
+            ts_pred = []
+            ts_labels = []
+            model.eval()
+            for i, (X_test, y_test) in enumerate(self.ts_loader):
+                pred = torch.nn.Softmax(dim=1)(model(X_test.to("cuda")))
+                pred = np.argmax(pred.cpu().detach().numpy(), axis=1)
+                ts_pred = np.r_[ts_pred, np.ravel(pred)]
+                ts_labels = np.r_[ts_labels, np.ravel(y_test.cpu())]
+            f1 = f1_score(y_true=ts_labels, y_pred=ts_pred, labels=np.unique(ts_labels), average='macro')
+            # f1 = F1Score(task="multiclass", num_classes=24)
+            # f1_score = f1(ts_pred, ts_labels)
+            print(f'Test Set F1: {f1}')
+                
 if __name__ == "__main__":
     # random_image_samples(data, num_rows=4, num_columns=4)
     # random_image_samples(tr_data.tr_data, num_rows=4, num_columns=4)  # color images only
@@ -224,13 +330,42 @@ if __name__ == "__main__":
     asl = ASLRecognition()
     model = asl.model_def()
     asl.fit(model)
+    # asl.test(model, used_saved=False)
+    asl.test(model, used_saved=True)
 
+#%%
 
+# Understanding Cross Entropy Calculation
+# https://medium.com/unpackai/cross-entropy-loss-in-ml-d9f22fc11fe0
 
+####################
+## CODE GRAVEYARD ##
+####################
 
+# # Get evaluation metrics for printing
+# tr_preds_array = []
+# tr_labels_array = []
+# val_preds_array = []
+# val_labels_array = []
+# # Get torch of predictions
+# tr_preds = torch.argmax(torch.nn.Softmax(dim=1)(tr_logits), axis=1)
+# val_preds = torch.argmax(torch.nn.Softmax(dim=1)(vl_logits), axis=1)
+# # Send prediction torches to numpy
+# tr_preds_np = np.argmax(tr_preds.cpu().detach().numpy())
+# val_preds_np = np.argmax(val_preds.cpu().detach().numpy())
+# # Create prediction and target arrays
+# tr_preds_array = np.r_[tr_preds_array, np.ravel(tr_preds_np)]
+# tr_labels_array = np.r_[tr_labels_array, np.ravel(y_train.cpu())]
+# val_preds_array = np.r_[val_preds_array, np.ravel(val_preds_np)]
+# val_labels_array = np.r_[val_labels_array, np.ravel(y_val.cpu())]
+# # Use prediciont and target array to calculate macro F1
+# f1_tr = round(f1_score(y_true=tr_labels_array, y_pred=tr_preds_array, labels=np.unique(tr_labels_array), average='macro'), 4)
+# f1_val = round(f1_score(y_true=val_labels_array, y_pred=val_preds_array, labels=np.unique(val_labels_array), average='macro'), 4)
+# # f1_tr = round(f1_macro(tr_preds_np, tr_labels), 4)
+# # f1_val = round(f1_macro(val_preds_np, vl_labels), 4)
+# acc_tr = torch.round(acc(tr_preds, tr_labels), 4)
+# acc_val = torch.round(acc(val_preds, vl_labels), 4)
+# train_loss_batch = round(loss_train / self.BATCH_SIZE, 5)
+# val_loss_batch = round(loss_val / self.BATCH_SIZE, 5)
 
-
-
-
-
-
+# %%
